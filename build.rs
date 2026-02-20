43c5c8a13721b179feda82660b5982bb2da2523b
kacpers <kacperek.szczesny@gmail.com>
2026-02-20 16:40:35 +0100

    refactor: static maps init

--- build.rs
+++ build.rs
@@ -1,3 +1,7 @@
+use std::collections::HashMap;
+use std::io::Write;
+use std::{fs::File, path::Path};
+
 use flate2::read::GzDecoder;
 
 static DB_ARCHIVE: &str = "https://corgi.cardog.io/vpic.lite.db.gz";
@@ -21,4 +25,69 @@ fn main() {
         let mut gz_decoder = GzDecoder::new(body_reader);
         std::io::copy(&mut gz_decoder, &mut file_writer).expect("reader writer copy failure");
     }
+
+    let out_dir = std::env::var("OUT_DIR").expect("OUT_DIR env variable not set");
+    let gen_path = Path::new(&out_dir).join("gen.rs");
+    if !gen_path.exists() {
+        let mut gen_file = File::create(&gen_path).expect("gen file create failure");
+        generate_wmi_make_map(&mut gen_file).expect("gen map failure");
+    }
+
+    let out_dir = std::env::var("OUT_DIR").expect("OUT_DIR env variable not set");
+    let gen_path = Path::new(&out_dir).join("gen_wmi_schema_id.rs");
+    if !gen_path.exists() {
+        let mut gen_file = File::create(&gen_path).expect("gen file create failure");
+        generate_wmi_schema_id_map(&mut gen_file).expect("gen map failure");
+    }
+}
+
+fn generate_wmi_make_map(file: &mut File) -> Result<(), std::io::Error> {
+    let wmi_make_csv = include_str!("assets/wmi_make.csv");
+    let wmi_make_inner_map = wmi_make_csv
+        .lines()
+        .skip(1)
+        .map(|line| line.split_once(',').expect("must have comma"))
+        .collect::<HashMap<_, _>>()
+        .into_iter()
+        .map(|(k, v)| format!(r#""{k}" => "{}","#, v.replace('"', "")))
+        .collect::<Vec<_>>()
+        .join("\n");
+
+    let wmi_make_map = format!(
+        r#"pub static WMI_MAKE_MAP: phf::Map<&'static str, &'static str> = phf::phf_map!{{{wmi_make_inner_map}}};"#
+    );
+    writeln!(file, "{}", wmi_make_map)?;
+    Ok(())
+}
+
+fn generate_wmi_schema_id_map(file: &mut File) -> Result<(), std::io::Error> {
+    let wmi_schema_id_csv = include_str!("assets/wmi_schema_id.csv");
+
+    let mut map: HashMap<String, Vec<String>> = HashMap::new();
+
+    for line in wmi_schema_id_csv.lines().skip(1) {
+        let (k, v) = line.split_once(',').expect("must have comma");
+        map.entry(k.to_string())
+            .or_default()
+            .push(v.replace('"', ""));
+    }
+
+    let wmi_schema_id_inner_map = map
+        .into_iter()
+        .map(|(k, values)| {
+            let values = values
+                .into_iter()
+                .map(|v| format!(r#""{v}""#))
+                .collect::<Vec<_>>();
+            let values = values.join(", ");
+            format!(r#""{k}" => &[{}],"#, values)
+        })
+        .collect::<Vec<_>>()
+        .join("\n");
+
+    let wmi_schema_id_map = format!(
+        r#"pub static WMI_SCHEMA_ID_MAP: phf::Map<&'static str, &'static [&'static str]> = phf::phf_map!{{{wmi_schema_id_inner_map}}};"#
+    );
+    writeln!(file, "{}", wmi_schema_id_map)?;
+    Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,9 @@ pub mod decoder;
 pub mod pattern;
 pub mod types;
 
+include!(concat!(env!("OUT_DIR"), "/gen.rs"));
+include!(concat!(env!("OUT_DIR"), "/gen_wmi_schema_id.rs"));
+
 pub type VIN = String;
 
 #[derive(Debug)]
@@ -19,5 +22,31 @@ pub enum CorgiError {
 impl From<sqlx::Error> for CorgiError {
     fn from(value: sqlx::Error) -> Self {
         Self::Sqlite(value)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_phf_wmi_make() {
+        let v = WMI_MAKE_MAP.get("WA1").expect("is not some");
+        assert_eq!(*v, "Audi")
+    }
+
+    #[test]
+    fn test_phf_wmi_schema_id() {
+        let v = WMI_SCHEMA_ID_MAP.get("WA1").expect("is not some");
+        assert_eq!(
+            *v,
+            &[
+                "3288", "3927", "3931", "3963", "3998", "4092", "4106", "4158", "4201", "4244",
+                "4265", "8341", "8389", "8471", "8483", "10930", "15507", "20427", "21297",
+                "22045", "23318", "24457", "24458", "24459", "24545", "24546", "24547", "24588",
+                "24767", "25233", "25830", "25831", "26029", "26031", "26032", "26642", "27226",
+                "27302", "27303", "27340", "27362", "27532", "28366", "28367", "28368", "28802"
+            ]
+        )
     }
 }
